606ba63939f8768b06efa3afde8edcc500d141aa
songxue77 <snowbrightly@hotmail.com>
2022-02-10 18:49:55 +0900

    fix(던파 > 설정): 키 숨김

--- config/df.php
+++ config/df.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'api_url' => 'https://api.neople.co.kr',
-    'api_key' => 'w06grw0LQkBfvq4inVgJ2HUsG1vTceM3'
+    'api_url' => env('DF_API_URL'),
+    'api_key' => env('DF_API_KEY')
 ];
